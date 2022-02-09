712afc78fe2b62a677828b86f1618ab529e3a078
Kevin Romero <a1299r5@gmail.com>
2022-02-09 14:52:20 -0600

    ✨ Create the film models

--- starwars_api/starwars/films/models.py
+++ starwars_api/starwars/films/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+
+    name = models.CharField(max_length=50)
+    characters = models.ManyToManyField('People')
