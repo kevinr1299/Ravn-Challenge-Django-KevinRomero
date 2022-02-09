361e836ff5f704657d59d9be870462e9b93b0c82
Kevin Romero <a1299r5@gmail.com>
2022-02-08 21:11:48 -0600

    t revert --no-edit 69704901451139ec2ebd43fb872148bb28b84c55 Revert "🎉 Configure basic project structure"
    This reverts commit 69704901451139ec2ebd43fb872148bb28b84c55.

--- starwars_api/setup.py
+++
@@ -1,9 +0,0 @@
-from ensurepip import version
-import setuptools
-
-setuptools.setup(
-    name='starwars_api',
-    version='0.0.0',
-    packages=setuptools.find_packages(),
-    scripts='manage.py',
-)
