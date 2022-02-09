b256970d2ae97e5bc39e73a96642c011b3655922
Kevin Romero <a1299r5@gmail.com>
2022-02-08 23:22:58 -0600

    ✨ Create a person model

---
+++ starwars_api/starwars/people/models/person.py
@@ -0,0 +1,26 @@
+from tabnanny import verbose
+from django.core.validators import MinValueValidator
+from django.db import models
+
+
+class Person(models.Model):
+
+    name = models.CharField(max_length=100)
+    height = models.FloatField(validators=[
+        MinValueValidator(limit_value=0),
+    ])
+    mass = models.FloatField(validators=[
+        MinValueValidator(limit_value=0),
+    ])
+    birth_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    hair_color = models.ForeignKey('HairColor', on_delete=models.PROTECT)
+    skin_color = models.ForeignKey('SkinColor', on_delete=models.PROTECT)
+    eye_color = models.ForeignKey('EyeColor', on_delete=models.PROTECT)
+    gender = models.ForeignKey('Gender', on_delete=models.PROTECT)
+    species = models.ManyToManyField('Specie')
+    homeworld = models.ForeignKey('World', on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name_plural = 'People'
