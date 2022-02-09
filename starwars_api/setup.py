b333bc3bbc1c3d6ac67ec6f2fd62e050304e23b1
Kevin Romero <a1299r5@gmail.com>
2022-02-08 21:14:28 -0600

    🎉 Configure basic backend-project structure

---
+++ starwars_api/setup.py
@@ -0,0 +1,9 @@
+from ensurepip import version
+import setuptools
+
+setuptools.setup(
+    name='starwars_api',
+    version='0.0.0',
+    packages=setuptools.find_packages(),
+    scripts='manage.py',
+)
