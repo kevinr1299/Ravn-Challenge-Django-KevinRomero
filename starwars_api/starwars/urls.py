5601f6a3b2217739e01f56cd3b9aa1d2116e63f4
Kevin Romero <a1299r5@gmail.com>
2022-02-09 23:25:13 -0600

    ✨ Include vehicle, film and person URL on global urls module

--- starwars_api/starwars/urls.py
+++ starwars_api/starwars/urls.py
@@ -14,8 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/people/', include(
+        ('starwars.people.urls.person', 'people'),
+        namespace='people',
+    )),
+    path('api/', include(
+        ('starwars.people.urls.catalogues', 'catalogues'),
+        namespace='catalogues',
+    )),
+    path('api/films/', include(
+        ('starwars.films.urls', 'films'),
+        namespace='films',
+    )),
+    path('api/vehicles/', include(
+        ('starwars.vehicles.urls', 'vehicles'),
+        namespace='vehicles',
+    )),
 ]
