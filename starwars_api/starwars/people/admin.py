0909369822dea72d4dfe83c98d5efbc9df548a75
Kevin Romero <a1299r5@gmail.com>
2022-02-09 23:27:50 -0600

    ✨ Register person on Django admin
    Create PersonAdmin to exclude created_at and updated_at fields

--- starwars_api/starwars/people/admin.py
+++ starwars_api/starwars/people/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from starwars.people.models.catalogues import (
+    EyeColor,
+    Gender,
+    HairColor,
+    SkinColor,
+    Specie,
+    World,
+)
+from starwars.people.models.person import Person
+
+
+class PersonAdmin(admin.ModelAdmin):
+
+    fields = (
+        'name',
+        'height',
+        'mass',
+        'birth_date',
+        'hair_color',
+        'skin_color',
+        'eye_color',
+        'gender',
+        'species',
+        'homeworld',
+        'vehicles',
+    )
+
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(HairColor)
+admin.site.register(SkinColor)
+admin.site.register(EyeColor)
+admin.site.register(Gender)
+admin.site.register(Specie)
+admin.site.register(World)
