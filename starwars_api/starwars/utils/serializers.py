ce1362137f74bf3dc6b48119902ba35d9f01313f
Kevin Romero <a1299r5@gmail.com>
2022-02-09 23:04:38 -0600

    ✨ Create a serializer generator
    Create a function that returns a model serializer with the sent model
    Override SlugRelatedField to define the name as slug_field

---
+++ starwars_api/starwars/utils/serializers.py
@@ -0,0 +1,41 @@
+from django.db.models import Model
+
+from rest_framework import serializers
+
+
+class NameRelatedField(serializers.SlugRelatedField):
+    '''
+        Create a slug serializer that returns a specific attribute of the model
+    '''
+
+    def __init__(self, slug_field: str = 'name', **kwargs):
+        super().__init__(slug_field=slug_field, read_only=True, **kwargs)
+
+
+def GenerateModelSerializer(
+    meta_model: Model,
+    view_name: str,
+) -> serializers.ModelSerializer:
+    '''
+        Return a model serializer with name and URL
+
+        Parameters:
+        meta_model(Model) A Django model
+        view_name(str) Name of the endpoint
+    '''
+
+    class CatalogueSerializer(serializers.ModelSerializer):
+
+        url = serializers.HyperlinkedIdentityField(
+            view_name=view_name,
+            read_only=True,
+        )
+
+        class Meta:
+            model = meta_model
+            fields = [
+                'name',
+                'url',
+            ]
+
+    return CatalogueSerializer
