ed4de75e041ccc13c70eca209cc6bd1717fd1b51
Kevin Romero <a1299r5@gmail.com>
2022-02-09 23:17:25 -0600

    ✨ Create a serializer for peope
    Inherit from a ModelSerializer
    Override the foreign keys
    Use the reverse relation to obtain the films

---
+++ starwars_api/starwars/people/serializers/person.py
@@ -0,0 +1,59 @@
+from rest_framework import serializers
+
+from starwars.people.models.person import Person
+from starwars.utils.serializers import NameRelatedField
+
+
+class PersonSerializer(serializers.ModelSerializer):
+
+    hair_color = NameRelatedField()
+    skin_color = NameRelatedField()
+    eye_color = NameRelatedField()
+    gender = NameRelatedField()
+    homeworld = serializers.HyperlinkedRelatedField(
+        read_only=True,
+        view_name='catalogues:world',
+    )
+    species = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='catalogues:specie',
+    )
+    vehicles = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='vehicles:vehicle',
+    )
+    films = serializers.HyperlinkedRelatedField(
+        source='film_set',
+        many=True,
+        read_only=True,
+        view_name='films:film',
+    )
+    url = serializers.HyperlinkedIdentityField(
+        view_name='people:person'
+    )
+
+    class Meta:
+        model = Person
+        fields = [
+            'name',
+            'height',
+            'mass',
+            'birth_date',
+            'created_at',
+            'updated_at',
+            'hair_color',
+            'skin_color',
+            'eye_color',
+            'gender',
+            'homeworld',
+            'species',
+            'vehicles',
+            'films',
+            'url',
+        ]
+        read_only_fields = [
+            'created_at',
+            'updated_at',
+        ]
