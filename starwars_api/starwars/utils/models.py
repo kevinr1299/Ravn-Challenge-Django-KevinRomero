1dfe6545466906b33b1fdcd8302ba8da7fc18c0a
Kevin Romero <a1299r5@gmail.com>
2022-02-11 00:08:07 -0600

    ✨ Create CatalogueManager
    Define a function to create n records in a specific table

--- starwars_api/starwars/utils/models.py
+++ starwars_api/starwars/utils/models.py
@@ -1,4 +1,36 @@
+from django.core.management.base import CommandError
+from django.db import IntegrityError
+from django.db.models import Model
+from faker import Faker
+
+fake = Faker()
+
+
 class PrintShowsName:
 
     def __str__(self) -> str:
         return self.name
+
+
+class CatalogueManager:
+
+    @classmethod
+    def seed_random(
+        cls,
+        model: Model,
+        table_name: str,
+        n_records: int,
+    ) -> list:
+        records = [
+            model(
+                name=fake.pystr(max_chars=10)
+            )
+            for _ in range(n_records)
+        ]
+        try:
+            return model.objects.bulk_create(records)
+        except IntegrityError as e:
+            raise CommandError(
+                'Bad luck, Faker repeated the value when creating '
+                f'{table_name} records',
+            ) from e
