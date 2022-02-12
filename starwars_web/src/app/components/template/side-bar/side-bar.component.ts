0a09f7edb334565bf3da89690949224d6fcb04a4
Kevin Romero <a1299r5@gmail.com>
2022-02-12 17:26:29 -0600

    ✨ Add click and load functionality
    Add a div to show an error loading
    Add a div to show load the data
    Implement the logic to select a person

--- starwars_web/src/app/components/template/side-bar/side-bar.component.ts
+++ starwars_web/src/app/components/template/side-bar/side-bar.component.ts
@@ -1,6 +1,8 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { PeopleSource } from 'src/app/datasources/peopleSource';
+import { Person } from 'src/app/interfaces/people';
 import { PersonService } from 'src/app/services/person.service';
 
 @Component({
@@ -8,15 +10,24 @@ import { PersonService } from 'src/app/services/person.service';
   templateUrl: './side-bar.component.html',
   providers: [],
   styleUrls: ['./side-bar.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SideBarComponent {
 
   dataSource!: PeopleSource;
+  personSelected: string = '';
 
   constructor(
     private personService: PersonService,
+    private router: Router,
   ) {
-    this.dataSource = new PeopleSource(this.personService);
-   }
+    this.dataSource = new PeopleSource(
+      this.personService,
+    );
+  }
+
+  changeSelectPerson(person: Person) {
+    this.personSelected = person.url;
+    this.personService.setCurrentPerson(person);
+    this.router.navigate(['person']);
+  }
 }
