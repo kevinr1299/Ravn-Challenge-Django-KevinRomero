57e3076421b0837efae0a44c0fe8f2409e3f51ae
Kevin Romero <a1299r5@gmail.com>
2022-02-12 17:22:02 -0600

    ✨ Set the methos to manage the current person selected

--- starwars_web/src/app/services/person.service.ts
+++ starwars_web/src/app/services/person.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
-import { People } from '../interfaces/people';
-import { Observable } from 'rxjs';
+import { People, Person } from '../interfaces/people';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +11,7 @@ import { Observable } from 'rxjs';
 export class PersonService {
 
   personURL = `${environment.api_url}/people/`
+  private _currentPerson = new BehaviorSubject<Person | null>(null);
 
   constructor(private http: HttpClient) { }
 
@@ -18,5 +19,13 @@ export class PersonService {
     return this.http.get<People>(
       `${this.personURL}?page=${page}`, {observe: 'response'}
     );
+  }
+
+  setCurrentPerson(person: Person) {
+    this._currentPerson.next(person);
+  }
+
+  getCurrentPerson() {
+    return this._currentPerson.asObservable();
   }
 }
