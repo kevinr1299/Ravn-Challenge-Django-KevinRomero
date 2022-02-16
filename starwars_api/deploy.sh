3cfcfb31ebd9e759c674e5aee36e44827d5a450d
Kevin Romero <a1299r5@gmail.com>
2022-02-16 10:38:35 -0600

    🚀 Add workwer-tmp-dir

--- starwars_api/deploy.sh
+++ starwars_api/deploy.sh
@@ -2,4 +2,4 @@
 python manage.py migrate;
 python manage.py collectstatic --noinput;
 python manage.py seedpeople 20;
-gunicorn --workers=4 --timeout 50 --bind=0.0.0.0:8000 starwars.wsgi;
+gunicorn --worker-tmp-dir=/dev/shm --workers=4 --timeout 50 --bind=0.0.0.0:8000 starwars.wsgi;
