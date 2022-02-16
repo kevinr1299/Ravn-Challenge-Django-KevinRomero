2e2e122250355191b08bf5d81693856df6fa3f3e
Kevin Romero <a1299r5@gmail.com>
2022-02-15 23:58:07 -0600

    🚀 Use dockerize

--- starwars_api/deploy.sh
+++ starwars_api/deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-until python manage.py migrate 2>/dev/null; do
-  echo 'Postgres is unavailable - sleeping' && sleep 1
-done
-
-echo 'success'
-
+python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py seedpeople 20
 gunicorn --workers=4 --timeout 50 --bind=0.0.0.0:8000 starwars.wsgi
