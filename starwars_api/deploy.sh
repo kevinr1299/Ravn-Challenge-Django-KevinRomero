a49fc406d1752b92e25d1a09dadeaffb1040156d
Kevin Romero <a1299r5@gmail.com>
2022-02-15 18:10:59 -0600

    🚀 Add deploy.sh file
    Delete CMD command from dockerfile
    Add command on web service to execute deploy.sh
    Add deploy.sh to do the project initialization

---
+++ starwars_api/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+until python manage.py migrate; do
+  echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+echo "success"
+
+python manage.py collectstatic --noinput
+python manage.py seedpeople 20
+gunicorn --workers=4 --timeout 50 --bind=0.0.0.0:8000 starwars.wsgi
